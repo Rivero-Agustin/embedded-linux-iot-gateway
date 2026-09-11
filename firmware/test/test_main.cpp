e835bff4698277f51bca69f221a46092b4ca883f
Agustin <agustinnicolas10@hotmail.com>
2026-09-11 17:33:32 -0300

    test: adapta test_main.cpp para Hardware-in-the-Loop

--- firmware/test/test_main.cpp
+++ firmware/test/test_main.cpp
@@ -1,20 +1,50 @@
 #include <unity.h>
 
-// Esta es la función que vamos a probar (podría ser tu algoritmo de UWB)
+// 1. La Lógica Pura que estamos testeando
 int calcular_riesgo_colision(int distancia) {
-    if (distancia < 100) return 1; // 1 = Peligro
-    return 0; // 0 = Seguro
+    if (distancia < 100) return 1;
+    return 0;
 }
 
-// Este es el test que evalúa si la función hace lo correcto
 void test_algoritmo_colision(void) {
     TEST_ASSERT_EQUAL(1, calcular_riesgo_colision(50));
     TEST_ASSERT_EQUAL(0, calcular_riesgo_colision(150));
 }
 
+// ==========================================
+// MAGIA MULTI-ENTORNO (Cloud vs Hardware)
+// ==========================================
+
+#if defined(ARDUINO)
+// ---> Si estamos flasheando el ESP32 (Hardware-in-the-loop)
+#include <Arduino.h>
+
+void setup() {
+    // Fundamental: Esperar 2 segundos para que Windows estabilice el puerto USB Serial
+    delay(2000); 
+    UNITY_BEGIN();
+    RUN_TEST(test_algoritmo_colision);
+    UNITY_END();
+}
+
+void loop() {
+    delay(100); // El test ya corrió, el loop se queda descansando
+}
+
+#elif defined(ESP_PLATFORM)
+// ---> Si estamos flasheando un ESP32 pero con ESP-IDF puro
+extern "C" void app_main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_algoritmo_colision);
+    UNITY_END();
+}
+
+#else
+// ---> Si estamos compilando en los servidores de GitHub (Ubuntu Native)
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_algoritmo_colision);
     UNITY_END();
     return 0;
 }
+#endif
